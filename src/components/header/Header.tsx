a09abd588b2733b8955a4411fb184c936eba0161
RostislavNaumenko <rostislavnaumenko.work@gmail.com>
2024-11-14 23:00:23 +0100

    fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import styles from './Header.module.css';
-import logo from  './berlin_small.png.png';
+import React from "react";
+import styles from "./Header.module.css";
+import logo from "./berlin_small.png";
 
 const Header: React.FC = () => {
   return (
@@ -9,23 +9,36 @@ const Header: React.FC = () => {
         <div className={styles.headerTop}>
           <div className={styles.leftSection}>
             <a href="#" className={styles.logoLink}>
-              <img src={logo} className={styles.headerLogo} alt="Travel Agency Logo" />
+              <img
+                src={logo}
+                className={styles.headerLogo}
+                alt="Travel Agency Logo"
+              />
             </a>
           </div>
           <div className={styles.rightSection}>
-          <a href="#" className={styles.personalAccount}>
-            Личный кабинет
-          </a>
+            <a href="#" className={styles.personalAccount}>
+              Личный кабинет
+            </a>
           </div>
         </div>
 
-        
         <div className={styles.headerBottom}>
           <div className={styles.navContainer}>
             <ul className={styles.headerList}>
-              {['О нас', 'Услуги', 'Преимущества', 'Туры', 'Отели', 'Контакты'].map((item, index) => (
+              {[
+                "О нас",
+                "Услуги",
+                "Преимущества",
+                "Туры",
+                "Отели",
+                "Контакты",
+              ].map((item, index) => (
                 <li className={styles.headerListItem} key={index}>
-                  <a href={`#${item.toLowerCase()}`} className={styles.headerLink}>
+                  <a
+                    href={`#${item.toLowerCase()}`}
+                    className={styles.headerLink}
+                  >
                     {item.toUpperCase()}
                   </a>
                 </li>
