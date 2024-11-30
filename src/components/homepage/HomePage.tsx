2769314bd53fa7dc466e533f46c8de4bb6e07b4f
RostislavNaumenko <rostislavnaumenko.work@gmail.com>
2024-11-30 21:59:27 +0100

    homepage

--- src/components/homepage/HomePage.tsx
+++ src/components/homepage/HomePage.tsx
@@ -1,29 +1,57 @@
 import React from "react";
 import styles from "./homepage.module.css";
-import image1 from "./photo/1.jpg";
-import image2 from "./photo/2.jpg";
-import image3 from "./photo/3.jpg";
+import imageSpetial1 from "./photo/1.jpg";
+import imageSpetial2 from "./photo/2.jpg";
+import imageSpetial3 from "./photo/3.jpg";
+import imageService1 from "./photo/service_1.jpg";
+import  imageService2 from "./photo/service_2.jpg";
+import  imageService3 from "./photo/service_3.jpg";
+import  imageService4 from "./photo/service_4.jpg";
+import { title } from "process";
 
 const HomePage: React.FC = () => {
   const cards = [
     {
-      image: image1,
+      image: imageSpetial1,
       title: "Экскурсии",
       buttonText: "Подробнее",
     },
     {
-      image: image2,
+      image: imageSpetial2,
       title: "Экстримальные походы",
       buttonText: "Подробнее",
     },
     {
-      image: image3,
+      image: imageSpetial3,
       title: "Где кататься этой зимой",
       buttonText: "Подробнее",
     },
   ];
 
+  const services = [
+    {
+      image: imageService1,
+      title: "ПРЕМИАЛЬНОЕ ОБСЛУЖИВАНИЕ",
+    },
+    {
+      image: imageService2,
+      title: "УСЛУГИ ГИДОВ",
+    },
+    {
+      image: imageService3,
+      title: "НАШИ БАССЕЙНЫ",
+    },
+    {
+      image: imageService4,
+      title: "ПРОКАТ ТРАНСПОРТ",
+    },
+  ];
+
+  
+
+
   return (
+    <div className={styles.body}>
     <div className={styles.offers}>
       <div className={styles.offersTitle}>Спецпредложения</div>
       <div className={styles.cardContainer}>
@@ -31,13 +59,31 @@ const HomePage: React.FC = () => {
           <div className={styles.card} key={index}>
             <img src={card.image} alt={card.title} className={styles.image} />
             <div className={styles.cardContent}>
-              <h2 className={styles.title}>{card.title}</h2>
+              <h2 className={styles.cardTitle}>{card.title}</h2>
               <button className={styles.cardButton}>{card.buttonText}</button>
             </div>
           </div>
         ))}
       </div>
     </div>
+    <div className={styles.services}>
+      <h2 className={styles.servicesTitle}>УСЛУГИ</h2>
+      <div className={styles.servicesContainer}>
+        {services.map((service, index) => (
+          <div className={styles.serviceCard} key={index}>
+            <img
+              src={service.image}
+              alt={service.title}
+              className={styles.serviceImage}
+            />
+            <div className={styles.serviceCardTitle}>{service.title}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+    </div>
+
+    
   );
 };
 
